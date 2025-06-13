a67f93a37cee0fb081db9184369db778c6ed54fd
gyuhyeon6201 <rbgus303@gmail.com>
2025-06-13 13:21:24 +0900

    create react flash project (0613)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-
+import "./App.scss";
+import { useState } from "react";
+import mockData from "./data/mockData";
+import Category from "./Components/Category";
+import FlashCard from "./Components/FlashCard";
+import CardLearn from "./Components/CardLearn";
+import Result from "./Components/Result";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    const [category, setCategory] = useState(null);
+    const [learningMode, setLearningMode] = useState(false);
+    const [exitMode, setExitMode] = useState(false);
+    const handleCategory = (obj) => {
+        setCategory(obj);
+    };
+    const handleLearn = (data)=>{
+      setLearningMode(data);
+    }
+    const handleExit = (mode)=>{
+      setExitMode(mode);
+    }
+    // category 정보를 가져옵니다.
+    return (
+        <div>
+            {!category && (
+              <Category
+                cardData={mockData.categories}
+                onSelected={handleCategory}
+              />
+            )}
+            {category && !learningMode &&(
+              <FlashCard cardData={category}
+              onSelected={handleCategory} 
+              onLearn={handleLearn} />
+            )}
+            {
+              learningMode && !exitMode &&
+              <CardLearn cardData={category.flashcards}
+              onExit={handleExit}/>
+            }
+            {
+              exitMode &&
+              <Result onExit={handleCategory}/>
+            }
+        </div>
+    );
 };
 
 export default App;
