5937c3df6d5eb79c263897baabb5b9f2f81eb5f0
gyuhyeon6201 <rbgus303@gmail.com>
2025-06-16 09:59:28 +0900

    create react flash project (0613)

--- src/App.js
+++ src/App.js
@@ -13,35 +13,39 @@ const App = () => {
     const handleCategory = (obj) => {
         setCategory(obj);
     };
-    const handleLearn = (data)=>{
-      setLearningMode(data);
-    }
-    const handleExit = (mode)=>{
-      setExitMode(mode);
-    }
+    const handleLearn = (data) => {
+        setLearningMode(data);
+    };
+    const handleExit = (mode) => {
+        setExitMode(mode);
+    };
     // category 정보를 가져옵니다.
     return (
         <div>
             {!category && (
-              <Category
-                cardData={mockData.categories}
-                onSelected={handleCategory}
-              />
+                <Category
+                    cardData={mockData.categories}
+                    onSelected={handleCategory}
+                />
             )}
-            {category && !learningMode &&(
-              <FlashCard cardData={category}
-              onSelected={handleCategory} 
-              onLearn={handleLearn} />
+            {category && !learningMode && (
+                <FlashCard
+                    cardData={category}
+                    onSelected={handleCategory}
+                    onLearn={handleLearn}
+                />
             )}
-            {
-              learningMode && !exitMode &&
-              <CardLearn cardData={category.flashcards}
-              onExit={handleExit}/>
-            }
-            {
-              exitMode &&
-              <Result onExit={handleCategory}/>
-            }
+            {learningMode && !exitMode && (
+                <CardLearn cardData={category.flashcards} onExit={handleExit} />
+            )}
+            {exitMode && (
+                <Result onExit={() => {
+                        setCategory(null);
+                        setLearningMode(false);
+                        setExitMode(false);
+                    }}
+                />
+            )}
         </div>
     );
 };
